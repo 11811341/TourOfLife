286b5fed95b0986a688da15ee5882551e22e469c
Patrik Szabó <e11811341@student.tuwien.ac.at>
2021-12-06 23:23:01 +0100

    added 3d cursor

--- src/app/components/Life3D/Life3D.component.ts
+++ src/app/components/Life3D/Life3D.component.ts
@@ -6,6 +6,7 @@ import {Color, GridHelper, Vector3} from 'three';
 import {Cell3D} from '../../Cell3D';
 import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls';
 import {Grid3D} from '../../Grid3D';
+import {findCachePath} from '@angular-devkit/build-angular/src/utils/cache-path';
 
 
 @Component({
@@ -33,9 +34,11 @@ export class Life3DComponent implements OnInit{
 
   private raycaster = new THREE.Raycaster();
   private raycast_plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({color: 0xffff00}));
+  private pointer;
 
   private paint_level: number = 1;
 
+  private edit_mode: boolean = false;
 
   ngOnInit(): void{console.log("test");
     let parent: string = "render_window";
@@ -54,8 +57,8 @@ export class Life3DComponent implements OnInit{
       that.mouse_down = false;
     }, false);
 
-
-    // this.raycast_plane.material.colorWrite = false;
+    this.raycast_plane.material.colorWrite = false;
+    this.raycast_plane.material.transparent = true;
     this.renderer.getRenderer().addEventListener('mousedown', function(e) {
       that.mouse_down = true;
 
@@ -67,10 +70,22 @@ export class Life3DComponent implements OnInit{
 
     }, false);
 
+    const geometry = new THREE.IcosahedronGeometry(0.05, 1);
+    const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    this.pointer = new THREE.Mesh( geometry, material );
+    this.scene.add(this.pointer);
+
+
     this.renderer.getRenderer().addEventListener('mousemove', function(e) {
       const rect = that.renderer.getRenderer().getBoundingClientRect();
       that.mouse.x = ( ( e.clientX - rect.left ) / ( rect.right - rect.left ) ) * 2 - 1;
       that.mouse.y = - ( ( e.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
+
+      that.raycast_plane.quaternion.copy(that.renderer.getCamera().quaternion);
+
+
+      that.cursorUpdate();
+
 
       if (that.mouse_down) {
         if (e.which == 1) {
@@ -92,6 +107,33 @@ export class Life3DComponent implements OnInit{
       }
     }, false);
 
+    window.addEventListener("keydown", function(e){
+      if(e.key == "e"){
+        that.edit_mode = !that.edit_mode;
+        if(that.edit_mode){
+          that.trackball.noZoom = true;
+        }else{
+          that.trackball.noZoom = false;
+        }
+      }
+    }, false);
+
+    window.addEventListener("wheel", function(e){
+        // that.pointer.position.z += Math.sign(event.deltaY) * 0.5;
+      if(that.edit_mode) {
+        var lookAtVector = new THREE.Vector3();
+        lookAtVector.x = that.pointer.position.x - that.renderer.getCamera().position.x;
+        lookAtVector.y = that.pointer.position.y - that.renderer.getCamera().position.y;
+        lookAtVector.z = that.pointer.position.z - that.renderer.getCamera().position.z;
+        lookAtVector = lookAtVector.normalize();
+        lookAtVector = lookAtVector.multiplyScalar(-1*(Math.sign(e.deltaY) * 0.05));
+        that.raycast_plane.position.x += lookAtVector.x;
+        that.raycast_plane.position.y += lookAtVector.y;
+        that.raycast_plane.position.z += lookAtVector.z;
+      }
+      that.cursorUpdate();
+    }, false);
+
 
     this.helperGrid = new THREE.GridHelper(100, 1000, new THREE.Color(0x888888));
     // this.helperGrid = new THREE.GridHelper(100, 1000);
@@ -108,6 +150,15 @@ export class Life3DComponent implements OnInit{
     // }, false);
   }
 
+  private cursorUpdate(){
+    const intersects = this.raycaster.intersectObject(this.raycast_plane);
+    if (intersects.length != 0) {
+      this.pointer.position.x = intersects[0].point.x;
+      this.pointer.position.y = intersects[0].point.y;
+      this.pointer.position.z = intersects[0].point.z;
+    }
+  }
+
   private check_addition() {
     // if (this.scene.children.length > 2) {  //if scene contains more that 2 objects(grid and plane) that means a cell is in place and can be deleted
     //   this.scene.children.splice(0, 2);
@@ -118,14 +169,20 @@ export class Life3DComponent implements OnInit{
     //     return;
     //   }
     // }
-    const intersects = this.raycaster.intersectObject(this.raycast_plane);
-    if (intersects.length != 0) {
-      let x = intersects[0].point.x < 0 ? Math.trunc(intersects[0].point.x * 10) - 1 : Math.trunc(intersects[0].point.x * 10) + 1;
-      let y = intersects[0].point.y < 0 ? Math.trunc(intersects[0].point.y * 10) - 1 : Math.trunc(intersects[0].point.y * 10) + 1;
+    // const intersects = this.raycaster.intersectObject(this.raycast_plane);
+    // if (intersects.length != 0) {
+    //   let x = intersects[0].point.x < 0 ? Math.trunc(intersects[0].point.x * 10) - 1 : Math.trunc(intersects[0].point.x * 10) + 1;
+    //   let y = intersects[0].point.y < 0 ? Math.trunc(intersects[0].point.y * 10) - 1 : Math.trunc(intersects[0].point.y * 10) + 1;
+    //
+    //   this.generate_cell(x, y, this.paint_level);
+    //   this.scene_reload();
+    // }
 
-      this.generate_cell(x, y, this.paint_level);
-      this.scene_reload();
-    }
+    let x = this.pointer.position.x < 0 ? Math.trunc(this.pointer.position.x * 10) - 1 : Math.trunc(this.pointer.position.x * 10) + 1;
+    let y = this.pointer.position.y < 0 ? Math.trunc(this.pointer.position.y * 10) - 1 : Math.trunc(this.pointer.position.y * 10) + 1;
+    let z = this.pointer.position.z < 0 ? Math.trunc(this.pointer.position.z * 10) - 1 : Math.trunc(this.pointer.position.z * 10) + 1;
+    this.generate_cell(x, y, z);
+    this.scene_reload();
   }
 
   private check_deletion() {
@@ -153,6 +210,9 @@ export class Life3DComponent implements OnInit{
     // if(!this.hide_grid)
       this.scene.add(this.helperGrid);
     this.scene.add(this.raycast_plane);
+
+    this.scene.add(this.pointer);
+
     // this.scene.background = this.bg_color;
     const cells = this.grid.get_cells();
     for (let c of cells) {
