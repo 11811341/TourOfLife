e87cbcf854fe407ae25354a447b5f6f590ca4dbc
Patrik Szabó <e11811341@student.tuwien.ac.at>
2021-12-07 23:20:50 +0100

    made 3d life functional

--- src/app/Grid3D.ts
+++ src/app/Grid3D.ts
@@ -1,17 +1,39 @@
 import {Cell3D} from './Cell3D';
+import * as THREE from 'three';
 
-
-export class Grid3D{
+export class Grid3D {
 
   private coords = [];
 
   private active = [];
 
+  private to_die = [];
+  private to_birth = [];
+
+  private lonely = [];
+  private overcrowded = [];
+
+  private restore = [];
+
+  private revert = [];
+
+  private revert_limit = 20;
+
+  private cell_color = new THREE.Color(0xffc107);
+
+  private prediction_mode: boolean = false;
+  private predicted_count: number = 0;
+
+  private min_survival: number = 5;
+  private max_survival: number = 7;
+  private birth: number = 6;
+
 
   public add_to_grid(cell: Cell3D, auto: boolean = false) {
 
-    if(this.coords[cell.getX()] && this.coords[cell.getX()][cell.getY()] && this.coords[cell.getX()][cell.getY()][cell.getZ()]!=null)
+    if (this.coords[cell.getX()] && this.coords[cell.getX()][cell.getY()] && this.coords[cell.getX()][cell.getY()][cell.getZ()] != null) {
       return;
+    }
 
     this.active.push(cell);
 
@@ -20,10 +42,12 @@ export class Grid3D{
     //   config+="["+a.getX()+","+a.getY()+"],";
     // console.log(config);
 
-    if (this.coords[cell.getX()] == null)
+    if (this.coords[cell.getX()] == null) {
       this.coords[cell.getX()] = [];
-    if(this.coords[cell.getX()][cell.getY()]==null)
-      this.coords[cell.getX()][cell.getY()]=[];
+    }
+    if (this.coords[cell.getX()][cell.getY()] == null) {
+      this.coords[cell.getX()][cell.getY()] = [];
+    }
     this.coords[cell.getX()][cell.getY()][cell.getZ()] = cell;
 
     // this.restore = [];
@@ -36,9 +60,171 @@ export class Grid3D{
     //     this.coords[x][y] = null;
     //   }
     // }
-    this.active.splice(this.active.indexOf(this.coords[x][y][z]),1);
-    this.coords[x][y][z]=null;
+    if (this.coords[x] && this.coords[x][y] && this.coords[x][y][z]) {
+      this.active.splice(this.active.indexOf(this.coords[x][y][z]), 1);
+      this.coords[x][y][z] = null;
+    }
   }
+
+
+  public advance() {
+    if (this.predicted_count > 0) {
+      this.active.splice(this.active.length - this.predicted_count);
+    }
+
+    this.to_die = [];
+    this.to_birth = [];
+    this.lonely = [];
+    this.overcrowded = [];
+
+    if (this.revert.length >= this.revert_limit) {
+      this.revert = this.revert.slice(1);
+    }
+
+    this.revert.push(this.active.slice());
+
+    for (let a of this.active) {
+      this.check_neighbors(a.getX(), a.getY(), a.getZ());
+    }
+
+    for (let death of this.to_die) {
+      this.active.splice(this.active.indexOf(death), 1);
+      this.coords[death.getX()][death.getY()][death.getZ()] = null;
+    }
+
+    for (let live of this.to_birth) {
+      this.active.push(live);
+      if (!this.coords[live.getX()]) {
+        this.coords[live.getX()] = [];
+      }
+      if (!this.coords[live.getX()][live.getY()]) {
+        this.coords[live.getX()][live.getY()] = [];
+      }
+      this.coords[live.getX()][live.getY()][live.getZ()] = live;
+    }
+
+    if (this.prediction_mode) {
+      // this.predict();
+    }
+  }
+
+  private check_neighbors(x: number, y: number, z: number) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        for (let k = -1; k <= 1; k++) {
+          const coord_x = (x < 0 && x + i == 0) ? 1 : ((x > 0 && x + i == 0) ? -1 : x + i);
+          const coord_y = (y < 0 && y + j == 0) ? 1 : ((y > 0 && y + j == 0) ? -1 : y + j);
+          const coord_z = (z < 0 && z + k == 0) ? 1 : ((z > 0 && z + k == 0) ? -1 : z + k);
+          if (this.coords[coord_x] && this.coords[coord_x][coord_y] && this.coords[coord_x][coord_y][coord_z]) {
+            this.cell_death(this.coords[coord_x][coord_y][coord_z]);
+          } else {
+            this.cell_birth(coord_x, coord_y, coord_z);
+          }
+        }
+      }
+    }
+  }
+
+  private cell_death(cell: Cell3D) {
+    let counter = 0;
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        for (let k = -1; k <= 1; k++) {
+          const coord_x = (cell.getX() < 0 && cell.getX() + i == 0) ? 1 : ((cell.getX() > 0 && cell.getX() + i == 0) ? -1 : cell.getX() + i);
+          const coord_y = (cell.getY() < 0 && cell.getY() + j == 0) ? 1 : ((cell.getY() > 0 && cell.getY() + j == 0) ? -1 : cell.getY() + j);
+          const coord_z = (cell.getZ() < 0 && cell.getZ() + k == 0) ? 1 : ((cell.getZ() > 0 && cell.getZ() + k == 0) ? -1 : cell.getZ() + k);
+          if (this.coords[coord_x] && this.coords[coord_x][coord_y] && this.coords[coord_x][coord_y][coord_z]) {
+            counter++;
+          }
+        }
+      }
+    }
+    if (!this.to_die.includes(cell)) {
+      if (counter < this.min_survival + 1) {
+        this.to_die.push(cell);
+        this.lonely.push(cell);
+      } else if (counter > this.max_survival + 1) {
+        this.to_die.push(cell);
+        this.overcrowded.push(cell);
+      }
+    }
+  }
+
+  private cell_birth(x: number, y: number, z: number) {
+    var counter = 0;
+    for (var i = -1; i <= 1; i++) {
+      for (var j = -1; j <= 1; j++) {
+        for (var k = -1; k <= 1; k++) {
+          var coord_x = (x < 0 && x + i == 0) ? 1 : ((x > 0 && x + i == 0) ? -1 : x + i);
+          var coord_y = (y < 0 && y + j == 0) ? 1 : ((y > 0 && y + j == 0) ? -1 : y + j);
+          var coord_z = (z < 0 && z + k == 0) ? 1 : ((z > 0 && z + k == 0) ? -1 : z + k);
+          if (this.coords[coord_x] && this.coords[coord_x][coord_y] && this.coords[coord_x][coord_y][coord_z]) {
+            counter++;
+          }
+        }
+      }
+    }
+    if (counter == this.birth) {
+      for (let cell of this.to_birth) {
+        if (cell.getX() == x && cell.getY() == y && cell.getZ() == z) {
+          return;
+        }
+      }
+      this.to_birth.push(new Cell3D(x, y, z, this.cell_color));
+    }
+  }
+
+  public clear_grid() {
+    this.restore = this.active;
+    this.active = [];
+    this.coords = [];
+    this.to_birth = [];
+    this.to_die = [];
+    this.revert = [];
+    this.lonely = [];
+    this.overcrowded = [];
+  }
+
+  public restore_grid() {
+    for (let c of this.restore) {
+      this.add_to_grid(c);
+    }
+  }
+
+  public revert_grid() {
+    if (this.revert.length > 0) {  //checks if any reverse steps exist
+      const prev = this.revert.pop();
+
+      //clear everything. computation starts fresh after a revert
+      this.active = [];
+      this.coords = [];
+      this.to_birth = [];
+      this.to_die = [];
+      this.restore = [];
+
+      //go through last state and add cells to grid
+      for (let c of prev) {
+        this.add_to_grid(c);
+      }
+    }
+  }
+
+  public setRevert(limit: number) {
+    this.revert_limit = limit;
+  }
+
+  public getRevert() {
+    return this.revert_limit;
+  }
+
+  public getToDie() {
+    return this.to_die;
+  }
+
+  public getToBirth() {
+    return this.to_birth;
+  }
+
 
   public get_cells() {
     return this.active;
--- src/app/components/Life3D/Life3D.component.ts
+++ src/app/components/Life3D/Life3D.component.ts
@@ -14,7 +14,7 @@ import {findCachePath} from '@angular-devkit/build-angular/src/utils/cache-path'
   templateUrl: './Life3D.component.html',
   styleUrls: ['./Life3D.component.css']
 })
-export class Life3DComponent implements OnInit{
+export class Life3DComponent implements OnInit {
 
   private scene = new THREE.Scene();
   protected renderer: Renderer3D;
@@ -26,7 +26,8 @@ export class Life3DComponent implements OnInit{
   running: boolean = false;
 
   private mouse = new THREE.Vector2();
-  private mouse_down: boolean = false;
+  private painting: boolean = false;
+  private deleting: boolean = false;
 
   private trackball;
 
@@ -36,12 +37,13 @@ export class Life3DComponent implements OnInit{
   private raycast_plane = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshBasicMaterial({color: 0xffff00}));
   private pointer;
 
-  private paint_level: number = 1;
+  // private paint_level: number = 1;
 
   private edit_mode: boolean = false;
 
-  ngOnInit(): void{console.log("test");
-    let parent: string = "render_window";
+  ngOnInit(): void {
+    console.log('test');
+    let parent: string = 'render_window';
     const width = document.getElementById(parent).offsetWidth;
     const height = document.getElementById(parent).offsetHeight;
 
@@ -54,95 +56,100 @@ export class Life3DComponent implements OnInit{
     }, false);
 
     this.renderer.getRenderer().addEventListener('mouseup', function(e) {
-      that.mouse_down = false;
+      if (e.button == 0) {
+        that.painting = false;
+      } else if (e.button == 2) {
+        that.deleting = false;
+      }
     }, false);
 
     this.raycast_plane.material.colorWrite = false;
     this.raycast_plane.material.transparent = true;
     this.renderer.getRenderer().addEventListener('mousedown', function(e) {
-      that.mouse_down = true;
-
       if (e.button == 0) {  //perform left click action -> add
+        that.painting = true;
+        that.deleting = false;
         that.check_addition();
       } else if (e.button == 2) {  //perform right click action -> delete
+        that.deleting = true;
+        that.painting = false;
         that.check_deletion();
       }
 
     }, false);
 
     const geometry = new THREE.IcosahedronGeometry(0.05, 1);
-    const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    this.pointer = new THREE.Mesh( geometry, material );
+    const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    this.pointer = new THREE.Mesh(geometry, material);
     this.scene.add(this.pointer);
 
 
     this.renderer.getRenderer().addEventListener('mousemove', function(e) {
       const rect = that.renderer.getRenderer().getBoundingClientRect();
-      that.mouse.x = ( ( e.clientX - rect.left ) / ( rect.right - rect.left ) ) * 2 - 1;
-      that.mouse.y = - ( ( e.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
+      that.mouse.x = ((e.clientX - rect.left) / (rect.right - rect.left)) * 2 - 1;
+      that.mouse.y = -((e.clientY - rect.top) / (rect.bottom - rect.top)) * 2 + 1;
 
       that.raycast_plane.quaternion.copy(that.renderer.getCamera().quaternion);
 
 
       that.cursorUpdate();
 
-
-      if (that.mouse_down) {
-        if (e.which == 1) {
-          that.check_addition();
-        } else if (e.which == 3) {
-          that.check_deletion();
-        }
+      if (that.painting) {
+        that.check_addition();
+      } else if (that.deleting) {
+        that.check_deletion();
       }
+
     }, false);
 
-    window.addEventListener("keydown", function(e){
-      if(e.key == "ArrowUp"){
-        console.log("up");
-        that.paint_level++;
-      }
-      else if(e.key == "ArrowDown"){
-        console.log("down");
-        that.paint_level--;
-      }
-    }, false);
+    // window.addEventListener('keydown', function(e) {
+    //   if (e.key == 'ArrowUp') {
+    //     console.log('up');
+    //     that.paint_level++;
+    //   } else if (e.key == 'ArrowDown') {
+    //     console.log('down');
+    //     that.paint_level--;
+    //   }
+    // }, false);
 
-    window.addEventListener("keydown", function(e){
-      if(e.key == "e"){
+    window.addEventListener('keydown', function(e) {
+      if (e.key == 'e') {
         that.edit_mode = !that.edit_mode;
-        if(that.edit_mode){
-          that.trackball.noZoom = true;
-        }else{
-          that.trackball.noZoom = false;
-        }
+        that.trackball.noZoom = that.edit_mode;
       }
     }, false);
 
-    window.addEventListener("wheel", function(e){
-        // that.pointer.position.z += Math.sign(event.deltaY) * 0.5;
-      if(that.edit_mode) {
+    window.addEventListener('wheel', function(e) {
+      // that.pointer.position.z += Math.sign(event.deltaY) * 0.5;
+      if (that.edit_mode) {
         var lookAtVector = new THREE.Vector3();
         lookAtVector.x = that.pointer.position.x - that.renderer.getCamera().position.x;
         lookAtVector.y = that.pointer.position.y - that.renderer.getCamera().position.y;
         lookAtVector.z = that.pointer.position.z - that.renderer.getCamera().position.z;
         lookAtVector = lookAtVector.normalize();
-        lookAtVector = lookAtVector.multiplyScalar(-1*(Math.sign(e.deltaY) * 0.05));
+        lookAtVector = lookAtVector.multiplyScalar(-1 * (Math.sign(e.deltaY) * 0.05));
         that.raycast_plane.position.x += lookAtVector.x;
         that.raycast_plane.position.y += lookAtVector.y;
         that.raycast_plane.position.z += lookAtVector.z;
       }
       that.cursorUpdate();
+
+      if (that.painting) {
+        that.check_addition();
+      } else if (that.deleting) {
+        that.check_deletion();
+      }
     }, false);
 
 
-    this.helperGrid = new THREE.GridHelper(100, 1000, new THREE.Color(0x888888));
-    // this.helperGrid = new THREE.GridHelper(100, 1000);
-    this.helperGrid.position.z = 0.001;
-    this.helperGrid.rotateOnAxis(new Vector3(1, 0, 0), 90 * Math.PI / 180);
+    // this.helperGrid = new THREE.GridHelper(100, 1000, new THREE.Color(0x888888));
+    // this.helperGrid.position.z = 0.001;
+    // this.helperGrid.rotateOnAxis(new Vector3(1, 0, 0), 90 * Math.PI / 180);
     this.trackball = new TrackballControls(this.renderer.getCamera(), this.renderer.getRenderer());
-    this.trackball.mouseButtons={LEFT: THREE.MOUSE.MIDDLE, RIGHT:null, MIDDLE: null};
-    this.trackball.rotateSpeed=2;
+    this.trackball.mouseButtons = {LEFT: THREE.MOUSE.MIDDLE, RIGHT: null, MIDDLE: null};
+    this.trackball.rotateSpeed = 2;
 
+    this.scene_reload();
     this.animate();
 
     // this.renderer.getRenderer().addEventListener('wheel', function(){
@@ -150,7 +157,7 @@ export class Life3DComponent implements OnInit{
     // }, false);
   }
 
-  private cursorUpdate(){
+  private cursorUpdate() {
     const intersects = this.raycaster.intersectObject(this.raycast_plane);
     if (intersects.length != 0) {
       this.pointer.position.x = intersects[0].point.x;
@@ -186,21 +193,15 @@ export class Life3DComponent implements OnInit{
   }
 
   private check_deletion() {
-    if (this.scene.children.length > 2) {  //if raycast detects more that 2 objects(grid and plane) that means a cell is in place and can be deleted
-      this.scene.children.splice(0, 2);
-      const cell_check = this.raycaster.intersectObjects(this.scene.children);
-      if (cell_check.length != 0) {
-        let x = cell_check[0].point.x < 0 ? Math.trunc(cell_check[0].point.x * 10) - 1 : Math.trunc(cell_check[0].point.x * 10) + 1;
-        let y = cell_check[0].point.y < 0 ? Math.trunc(cell_check[0].point.y * 10) - 1 : Math.trunc(cell_check[0].point.y * 10) + 1;
-
-        this.grid.remove_from_grid(x, y, this.paint_level);
-      }
-      this.scene_reload();
-    }
+    let x = this.pointer.position.x < 0 ? Math.trunc(this.pointer.position.x * 10) - 1 : Math.trunc(this.pointer.position.x * 10) + 1;
+    let y = this.pointer.position.y < 0 ? Math.trunc(this.pointer.position.y * 10) - 1 : Math.trunc(this.pointer.position.y * 10) + 1;
+    let z = this.pointer.position.z < 0 ? Math.trunc(this.pointer.position.z * 10) - 1 : Math.trunc(this.pointer.position.z * 10) + 1;
+    this.grid.remove_from_grid(x, y, z);
+    this.scene_reload();
   }
 
   generate_cell(x: number, y: number, z: number, auto: boolean = false) {
-    const cell = new Cell3D(x, y, z, new Color('rgb(255,0,0)'));
+    const cell = new Cell3D(x, y, z, new THREE.Color(0xffc107));
     this.grid.add_to_grid(cell, auto);
     this.scene_reload();
   }
@@ -208,10 +209,22 @@ export class Life3DComponent implements OnInit{
   scene_reload() {
     this.scene = new THREE.Scene();
     // if(!this.hide_grid)
-      this.scene.add(this.helperGrid);
+    // this.scene.add(this.helperGrid);
     this.scene.add(this.raycast_plane);
 
     this.scene.add(this.pointer);
+
+    // for(let i = 0; i<50; i++){
+    //   let test = new THREE.GridHelper(100, 1000, new THREE.Color(0x888888));
+    //   let test_b = new THREE.GridHelper(100, 1000, new THREE.Color(0x888888));
+    //   test.position.z = 0.001 + i*0.1;
+    //   test_b.position.z = 0.001 - i*0.1;
+    //   test.rotateOnAxis(new Vector3(1, 0, 0), 90 * Math.PI / 180);
+    //   test_b.rotateOnAxis(new Vector3(1, 0, 0), 90 * Math.PI / 180);
+    //   this.scene.add(test);
+    //   this.scene.add(test_b);
+    // }
+
 
     // this.scene.background = this.bg_color;
     const cells = this.grid.get_cells();
@@ -229,12 +242,45 @@ export class Life3DComponent implements OnInit{
 
     if (this.running) {
       this.delta += this.clock.getDelta();
+      if (this.delta > this.interval) {
+        this.grid.advance();
         // this.nr_to_die = this.grid.getToDie().length;
         // this.nr_to_birth = this.grid.getToBirth().length;
-        // this.scene_reload();
-      this.delta = this.delta % this.interval;
+        this.scene_reload();
+        this.delta = this.delta % this.interval;
+      }
     }
     this.renderer.render(this.scene);
   };
+
+
+  clear(){
+    this.grid.clear_grid();
+    this.scene_reload();
+  }
+
+  revert(){
+    this.grid.revert_grid();
+    this.scene_reload();
+  }
+
+  play(){
+    this.running = !this.running;
+  }
+
+  advance(){
+    if (!this.running) {
+      this.grid.advance();
+      this.scene_reload();
+    }
+    // this.nr_to_die = this.grid.getToDie().length;
+    // this.nr_to_birth = this.grid.getToBirth().length;
+  }
+
+  restore(){
+    this.grid.restore_grid();
+    this.scene_reload();
+  }
+
 
 }
